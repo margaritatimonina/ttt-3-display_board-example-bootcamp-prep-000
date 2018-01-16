c5cf795ff216d1b89e4eda3b436419aff7f1d42f
Margarita Timonina <margarita.timonina@gmail.com>
2018-01-16 00:41:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 // ♥ irb
-001:0 > def display_board
-002:1 >     puts "A Tic Tac Toe Board"
-003:1 >   end
+def display_board
+puts "A Tic Tac Toe Board"
+end
 => :display_board
 004:0 > display_board
 A Tic Tac Toe Board
